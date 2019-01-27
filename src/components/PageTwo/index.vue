908ed4603718321ff3fb7395b03b69cc31dec07c
stanislavskiy <stanislavskiy.sanya@gmail.com>
2019-01-27 20:34:33 +0200

    PageTwo component

---
+++ src/components/PageTwo/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <router-link :to="{name: 'pageOne'}">Page one</router-link>
+    <div class="titles">
+      <h3 
+        class="titles__item" 
+        v-for="title in titles"
+        :key="title"
+        >
+          {{title}}
+        </h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PageTwo",
+
+  asyncData({ store, route }) {
+    return store.dispatch("loadPhotos");
+  },
+
+  computed: {
+    titles() {
+      return this.$store.getters.photosTitles;
+    }
+  }
+};
+</script>
