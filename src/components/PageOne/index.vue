40020c9e7ce239b7e15c42590b738bc737f8412b
stanislavskiy <stanislavskiy.sanya@gmail.com>
2019-01-27 20:34:20 +0200

    PageOne component

---
+++ src/components/PageOne/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <router-link
+      class="link" 
+      :to="{name: 'pageTwo'}" 
+      @mouseenter.native="linkMouseEnter"
+    >
+      Page two (preload on mouseenter)
+    </router-link>
+
+    <router-link 
+      class="link"
+      :to="{name: 'pageTwo'}" 
+    >
+      Page two (no preload)
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PageOne",
+  methods: {
+    linkMouseEnter() {
+      this.$store.dispatch("loadPhotos");
+    }
+  }
+};
+</script>
+
+<style scoped>
+  .link {
+    display: block;
+    margin-bottom: 40px;
+  }
+</style>
