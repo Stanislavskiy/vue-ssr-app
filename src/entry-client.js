3a5b12e9d97a751cfe831a8f826ddaa0f1913887
stanislavskiy <stanislavskiy.sanya@gmail.com>
2019-01-27 20:22:49 +0200

    Client entry script

---
+++ src/entry-client.js
@@ -0,0 +1,42 @@
+import Vue from "vue";
+import { createApp } from "./main";
+
+// Resolves async data on router parameter change
+Vue.mixin({
+  beforeRouteUpdate(to, from, next) {
+    const { asyncData } = this.$options;
+
+    if (asyncData) {
+      asyncData({
+        store: this.$store,
+        route: to
+      })
+        .then(next)
+        .catch(next);
+    } else {
+      next();
+    }
+  }
+});
+
+// Resolves async data before mount
+Vue.mixin({
+  beforeMount() {
+    const { asyncData } = this.$options;
+    if (asyncData) {
+      this.dataPromise = asyncData({
+        store: this.$store,
+        route: this.$route
+      });
+    }
+  }
+});
+
+const { app, store } = createApp();
+
+if (window.__INITIAL_STATE__) {
+  // Set state that determined during SSR and inlined in the page markup
+  store.replaceState(window.__INITIAL_STATE__);
+}
+
+app.$mount("#app");
