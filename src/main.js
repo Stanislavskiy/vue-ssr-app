38cab92c24861cf1ec41effaba9088e96dff2190
stanislavskiy <stanislavskiy.sanya@gmail.com>
2019-01-27 20:21:14 +0200

    Update main entry

--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
-import Vue from "vue";
-import App from "./App.vue";
+import Vue from 'vue'
+import App from './App'
+import { createRouter } from './router'
+import { createStore } from './store'
+import { sync } from 'vuex-router-sync'
 
-new Vue({
-  el: "#app",
-  components: { App },
-  template: "<App/>"
-});
+export function createApp() {
+
+  const router = createRouter()
+  const store = createStore()
+
+  // Sync router state with vuex
+  sync(store, router)
+
+  const app = new Vue({
+    router,
+    store,
+    render: h => h(App)
+  })
+
+  return { app, router, store }
+}
