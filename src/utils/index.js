ff74c9093daa999f89a1661ed2ef9683404b507b
stanislavskiy <stanislavskiy.sanya@gmail.com>
2019-01-28 10:54:18 +0200

    Update method description

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,10 @@
 export function timeoutOver(time, timeout) {
   /* 
-    Check whether the timeout has expired in relation to the current time
+    Check whether the timeout has expired in 
+    relation to the current time.
+    Intended to synchronous timeout calculation, 
+    based on initial time and timeout value 
+    (in milliseconds).
   */
   if (time && timeout) {
     const timeNow = new Date().getTime();
